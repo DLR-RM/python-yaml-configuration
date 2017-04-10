956cd3cd3945f4bf54ddf7960200eb33409ba709
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-04-10 15:48:04 +0200

    Provide correct license
    - add LICENSE file (BSD)
    - change copyright header from EPL to BSD

--- python/yaml_configuration/config.py
+++ python/yaml_configuration/config.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2016-2017 DLR
 #
 # All rights reserved. This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License v1.0 which
-# accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
+# available under the terms of the 2-Clause BSD License ("Simplified BSD
+# License") which accompanies this distribution, and is available at
+# https://opensource.org/licenses/BSD-2-Clause
 #
 # Contributors:
 # Benno Voggenreiter <benno.voggenreiter@dlr.de>
